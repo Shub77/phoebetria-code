749135e18c93247a13b94b7a7974f131e816d8ef
Craig Robbins <craigrobbins@users.sf.net>
2012-08-03 08:42:00 +1000

    Added debugging/stub functions to make sure we're getting data from the device.

--- io-manager.cpp
+++ io-manager.cpp
@@ -3,9 +3,26 @@
 IOManager::IOManager(QObject *parent) :
     QObject(parent)
 {
+    m_io_device.connect(PHOEBETRIA_IO_BITFENIX_RECON_VENDORID,
+                        PHOEBETRIA_IO_BITFENIX_RECON_PRODUCTID);
+    qDebug(m_io_device.isConnected() ? "Connected" : "NOT connected");
+    connectSignals();
+}
+
+void IOManager::connectSignals(void)
+{
+    QObject::connect(&m_io_device, SIGNAL(dataRX(QByteArray)),
+                     this, SLOT(onRawData(QByteArray)));
 }
 
 void IOManager::onPollTimerTriggered(void)
 {
     qDebug("Poll");
+    m_io_device.pollForData();
+}
+
+void IOManager::onRawData(QByteArray rawdata)
+{
+    qDebug("Got raw data");
+    qDebug(rawdata.toHex());
 }
--- io-manager.h
+++ io-manager.h
@@ -2,7 +2,12 @@
 #define IOMANAGER_H
 
 #include <QObject>
+#include <QByteArray>
 #include "device-io.h"
+
+#define PHOEBETRIA_IO_BITFENIX_RECON "bitfenix-recon"
+#define PHOEBETRIA_IO_BITFENIX_RECON_VENDORID 3141
+#define PHOEBETRIA_IO_BITFENIX_RECON_PRODUCTID 28928
 
 class IOManager : public QObject
 {
@@ -14,9 +19,13 @@ signals:
     
 public slots:
     void onPollTimerTriggered(void);
+    void onRawData(QByteArray rawdata);
+
+protected:
+    void connectSignals(void);
 
 private:
-    static DeviceIO m_io_device;
+    DeviceIO m_io_device;
 };
 
 #endif // IOMANAGER_H
