c2e03e93b5f40529868b42b2cd42c694c728b678
Craig Robbins <craigrobbins@users.sf.net>
2012-08-18 09:47:33 +1000

    Changed the way min settable fan speed is calculated.

--- gui_mainwindow.cpp
+++ gui_mainwindow.cpp
@@ -493,7 +493,7 @@ void gui_MainWindow::userChangedChannelRpmSlider(int channel, int value)
 int gui_MainWindow::rpmSliderValueToRPM(int channel, int value) const
 {
     int channelMaxRPM = m_fcd.maxRPM(channel);
-    int channelMinRPM = (int)(channelMaxRPM * 0.50 / 100) * 100;
+    int channelMinRPM = floor(channelMaxRPM * 0.50 / 100) * 100;
 
     double val = value / 100.0 * channelMaxRPM;
     val = int(val / 100) * 100;
