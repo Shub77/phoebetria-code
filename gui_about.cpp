28c512b48906b45905fd4d5980a94543c7206d3d
Craig Robbins <craigrobbins@users.sf.net>
2012-08-18 12:23:15 +1000

    Updated about dialog.

--- gui_about.cpp
+++ gui_about.cpp
@@ -1,11 +1,15 @@
 #include "gui_about.h"
 #include "ui_gui_about.h"
 
+#include "builddetails.h"
+
 gui_About::gui_About(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::gui_About)
 {
     ui->setupUi(this);
+    ui->ctrl_version->setText("Version " + BuildDetails::versionStr());
+    ui->ctrl_build->setText("Build date: " + BuildDetails::buildDateTimeStr());
 }
 
 gui_About::~gui_About()
